3ccffd2ffde10f4effdf26e2bbe08dff5c1b7fac
Aniket Banerjee <aniketbanerjee8334845906@gmail.com>
2026-09-12 09:30:24 +0530

    new changes

--- frontend/src/pages/Purchase/AllPurchaseList.jsx
+++ frontend/src/pages/Purchase/AllPurchaseList.jsx
@@ -327,7 +327,7 @@ export default function AllPurchaseList() {
         }}
       > */}
         <div
-        className="flex flex-col bg-white"
+        className="all-purchase-page flex flex-col bg-white"
         style={{
           height: "100%",
           minHeight: 0,
